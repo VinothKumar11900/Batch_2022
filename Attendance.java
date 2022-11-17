e76954fa93314b7a8051873542149f2eee7ae245
vinoth <mailtovino11900@gmail.com>
2022-11-17 15:52:24 +0530

    "Sysout bye added"

--- Attendance.java
+++ Attendance.java
@@ -2,6 +2,6 @@ public class Attendance {
     public static void main(String[] args) {
         int a =10;
         System.out.println("Hello");
-        
+        System.out.println("Bye");
     }
 }
