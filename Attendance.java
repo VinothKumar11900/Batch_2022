6b89a38e6910319b184f231e3ac7a82cd5d1428f
vinoth <mailtovino11900@gmail.com>
2022-11-17 16:50:42 +0530

    variable c is added

--- Attendance.java
+++ Attendance.java
@@ -1,7 +1,8 @@
 public class Attendance {
     public static void main(String[] args) {
         int a =10;
-        int b=20;
+        int b=200;
+        int c=80;
         System.out.println("Hello");
         System.out.println("Bye");
     }
