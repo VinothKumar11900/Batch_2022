0f553cc201cae7e06fef77ab23c47b8e53924214
pradeepkumar170301 <pradeepkumar@nmsworks.co.in>
2022-11-17 17:24:39 +0530

    variables deleted

--- Attendance.java
+++ Attendance.java
@@ -1,7 +1,6 @@
 public class Attendance {
     public static void main(String[] args) {
         int a =10;
-        int b=200;
         System.out.println("Hello");
         System.out.println("Bye");
     }
