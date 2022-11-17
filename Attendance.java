c2415a2e3347d9d2f54c11f4af7f42600a2e21ca
pradeepkumar170301 <pradeepkumar@nmsworks.co.in>
2022-11-17 17:15:23 +0530

    c is removed

--- Attendance.java
+++ Attendance.java
@@ -2,7 +2,6 @@ public class Attendance {
     public static void main(String[] args) {
         int a =10;
         int b=200;
-        int c=80;
         System.out.println("Hello");
         System.out.println("Bye");
     }
